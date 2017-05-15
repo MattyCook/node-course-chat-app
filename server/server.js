e5effaf2c50d0e93c829435dda5aa6606a17cc3b
MattyCook <m.kuchar00@gmail.com>
2017-05-15 22:23:38 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,41 +14,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
-    console.log('create message: ', message);
+    console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
